2042cd23341874cc8efb2a33b0192552698c6735
vonSchalienR <vonschalienrosa@gmail.com>
2025-11-26 19:55:44 +0200

    Deleting all
    Co-Authored-By: oonahenriikka <144310036+oonahenriikka@users.noreply.github.com>

--- lib/appwrite.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from 'react-native-appwrite';
-
-const client = new Client()
-.setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
-.setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
-.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME!);
-
-export const account = new Account(client);
