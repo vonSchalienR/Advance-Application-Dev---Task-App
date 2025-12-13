4ab9185bfb3d60fa9e254d8a66215ee5c48e6a41
Jiaoaqua <157594316+Jiaoaqua@users.noreply.github.com>
2025-12-13 23:13:34 +0200

    Fix gesture handler setup and stabilize TaskItem swipe

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./src/contexts/AuthContext";
 import { ThemeProvider, useThemeContext } from "./src/contexts/ThemeContext";
@@ -20,14 +22,17 @@ function RootNavigation() {
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider>
-        <PaperProvider>
-          <AuthProvider>
-            <RootNavigation />
-          </AuthProvider>
-        </PaperProvider>
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <PaperProvider>
+            <AuthProvider>
+              <RootNavigation />
+            </AuthProvider>
+          </PaperProvider>
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
+
